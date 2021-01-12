611722e5126ffc28120ff796ac77504e40c4bdb0
ambivert-sam <samwasu999@gmail.com>
2021-01-12 18:22:03 +0530

    Testing project

---
+++ mad_libs_game.py
@@ -0,0 +1,8 @@
+color=input("Enter any color name: ")
+plural_noun=input("Enter any noun: ")
+celebrity=input("Enter your favorite celebrity : ")
+
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love "+ celebrity)
